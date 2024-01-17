5b0dfb7685f8641f3711f2bd61aacd6428b9bd12
Moshentsev Nikita <trane0909@bk.ru>
2024-01-17 23:04:30 +0300

    create project

---
+++ BNETTest/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  BNETTest
+//
+//  Created by Никита Мошенцев on 17.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
